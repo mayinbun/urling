baae159763c320e8fddf0496e64e948f80f1a4a1
Bunyamin Yildirim <bunyamin.gs@gmail.com>
2019-04-19 13:48:08 +0200

    init

---
+++ README.md
@@ -0,0 +1,48 @@
+#Urling
+Zero dependency utility library to check if a url is available.
+
+Use case example: wait for a local server to start on http://localhost:8080 before doing some stuff in CI/CD.
+Rather then scheduling a long wait/sleep, checking on availability might speed up things.
+###Installation
+
+```
+npm install urling
+```
+
+###Usage (with default options)
+```js
+const urling = require('urling');
+
+urling('https://google.com')
+.then((status) => {
+	// ping success
+	console.log('statusCode', status)
+}).catch((rejection) => {
+	// ping failure
+})
+```
+
+###With custom options
+
+- required [string] url: the url to check for availability
+- optional [number] retry: amount of retries (default is 3)
+- optional [number] interval: optional interval wait time (default is 5000 ms)
+- optional [boolean] immediate: triggers before waiting (default is false)
+
+```js
+const urling = require('urling');
+
+const options = {
+	url: 'https://google.com',
+	retry: 6,
+	interval: 10000,
+	immediate: true
+}
+
+urling(options)
+.then((response) => {
+	// ping success
+}).catch((rejection) => {
+	// ping failure
+})
+``` 
---
+++ index.js
@@ -0,0 +1,62 @@
+const http = require('http');
+const https = require('https');
+
+const defaultOptions = {
+	retry: 3,
+	interval: 5000,
+	immediate: false
+};
+
+async function waitForUrl(options = { url: '' }) {
+	const _options = defaultOptions;
+	let retryCount = 0;
+	let available = false;
+
+	if (typeof options === 'string') _options.url = options;
+
+	else Object.assign(_options, options);
+
+	return new Promise((resolve, reject) => {
+		function init() {
+			if (_options.immediate) execute() && retryCount--;
+			const interval = setInterval(execute, _options.interval);
+
+			async function execute() {
+				console.info(`waiting for [${ _options.url }] to be available...`);
+
+				available = await ping(_options.url);
+
+				retryCount++;
+
+				if (available) {
+					clearInterval(interval);
+					resolve(available);
+					console.info(`ping to [${ _options.url }] successful`);
+				}
+
+				if (retryCount === _options.retry) {
+					clearInterval(interval);
+					reject(available);
+					console.info(`retried ${ _options.retry } times with no success`);
+				}
+			}
+		}
+
+		init()
+	});
+}
+
+async function ping(url) {
+	const isHttps = url.startsWith('https');
+	const protocol = isHttps ? https : http;
+
+	return new Promise((resolve, reject) => {
+		return protocol.get(url, (response) => {
+			return resolve(response.statusCode);
+		}).on('error', (e) => {
+			return resolve(0);
+		})
+	})
+}
+
+module.exports = waitForUrl;
